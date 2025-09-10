b99fcb5ba3dea17cf2261b68dfb62831e2bd92d3
DerC <sleechengn@live.cn>
2025-09-10 13:28:59 +0000

    保存程序

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -12,15 +12,21 @@ export default class Index extends React.Component {
                     state: any = {
                         time: undefined
                     };
+                    timer: any = undefined;
                     getTime() {
                         let date = new Date();
                         return `${date.getFullYear()}年${date.getMonth()}月${date.getDay()}日${date.getHours()}时${date.getMinutes()}分${date.getSeconds()}秒`;
                     }
 
                     componentDidMount(): void {
-                        setInterval(() => {
+                        this.timer = setInterval(() => {
                             this.setState({ time: this.getTime() });
                         }, 1000);
+                    }
+                    componentWillUnmount(): void {
+                        if (this.timer) {
+                            clearInterval(this.timer);
+                        }
                     }
                     render(): React.ReactNode {
                         return <>{this.state.time}</>
