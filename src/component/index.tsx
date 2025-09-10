9e51c3ea1e80b5ec4f4892219cbdb6ad32cda591
DerC <sleechengn@live.cn>
2025-09-10 12:35:39 +0000

    保存

--- src/component/index.tsx
+++ src/component/index.tsx
@@ -1,16 +1,28 @@
 import * as React from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 
+const GContext = React.createContext<any>({});
+
 export default class Index extends React.Component {
 
     render(): React.ReactNode {
         return <>
-            <BrowserRouter>
-                <Routes>
-                    <Route path="/" element={<div>首页</div>}></Route>
-                    <Route path="/about" element={<div>静态网站模板</div>}></Route>
-                </Routes>
-            </BrowserRouter>
+            <GContext.Provider value={{}}>
+                <BrowserRouter>
+                    <Routes>
+                        <Route path="/" element={<div>首页</div>} />
+                        <Route path="/about" element={<div>静态网站模板</div>} />
+                    </Routes>
+                </BrowserRouter>
+                <>
+                    <GContext.Consumer>
+                        {value => {
+                            console.log(`context: ${value}`);
+                            return <></>
+                        }}
+                    </GContext.Consumer>
+                </>
+            </GContext.Provider>
         </>
     }
 
