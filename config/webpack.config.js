d868a8429cecf2e85bb6b700543f4b96facf9b1d
DerC <sleechengn@live.cn>
2025-09-10 09:47:01 +0000

    保存

---
+++ config/webpack.config.js
@@ -0,0 +1,44 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: "development",
+    entry: {
+        app: [
+            path.join(__dirname, '../src/index.tsx')
+        ]
+    },
+    output: {
+        filename: '[name].js',
+        path: path.join(__dirname, '../build'),
+        publicPath: '/'
+    },
+    module: {
+        rules: [
+            {
+                test: /\.tsx?$/,
+                use: [{loader: 'ts-loader'}]
+            }
+        ]
+    },
+    devServer: {
+        port: 7000,
+        static:{
+            directory: path.join(__dirname, '../build/'),
+        },
+        host: "0.0.0.0",
+        compress: true,
+        hot: true,
+        historyApiFallback: true
+    },
+    resolve: {
+        //下面后缀的文件导入时可以省略文件名，js必须要有，否则会react.js文件会无法被解析
+        extensions: [".ts", ".tsx", ".js", ".jsx", ".vue", ".css", ".less", ".scss"]
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+            output:path.join(__filename,"../build/index.html"),
+            template:path.relative(__filename,"../src/index.html")
+        })
+    ]
+};
---
+++ src/component/index.tsx
@@ -0,0 +1,10 @@
+import * as React from "react";
+
+
+export default class Index extends React.Component {
+
+    render(): React.ReactNode {
+        return <h1 style={{ backgroundColor: "#00FF00", textAlign: "center" }}>hello world!</h1>;
+    }
+
+}
